8857f5d1b5a9697db0c42ee38147731c61876545
Henrique Wolf <henriquewolf1999@gmail.com>
2025-05-14 16:55:18 -0300

    define processor as a class

--- processor.cpp
+++ processor.cpp
@@ -1,59 +1,26 @@
-#include <iostream>
-#include <string>
-#include <map>
 #include <bitset>
-
-#define MAX_INST_MEM_SIZE 1024
-#define MAX_DATA_MEM_SIZE 65536
-
-uint32_t inst_memory[MAX_INST_MEM_SIZE];
-uint32_t data_memory[MAX_DATA_MEM_SIZE];
-
-// saves the values of all registers available
-typedef struct {
-    unsigned address;
-    unsigned value;
-} Register;
-
-static Register registers[] = 
-{
-    {0, 0x00},     // $0 - constant 0
-    {1, 0x00},     // $at - assembler temporary
-    {2, 0x00},     // $v0
-    {3, 0x00},     // $v1
-    {4, 0x00},     // $a0
-    {5, 0x00},     // $a1
-    {6, 0x00},     // $a2
-    {7, 0x00},     // $a3
-    {8, 0x00},     // $t0
-    {9, 0x00},     // $t1
-    {10, 0x00},    // $t2
-    {11, 0x00},    // $t3
-    {12, 0x00},    // $t4
-    {13, 0x00},    // $t5
-    {14, 0x00},    // $t6
-    {15, 0x00},    // $t7
-    {16, 0x00},    // $s0
-    {17, 0x00},    // $s1
-    {18, 0x00},    // $s2
-    {19, 0x00},    // $s3
-    {20, 0x00},    // $s4
-    {21, 0x00},    // $s5
-    {22, 0x00},    // $s6
-    {23, 0x00},    // $s7
-    {24, 0x00},    // $t8
-    {25, 0x00},    // $t9
-    {26, 0x00},    // $k0
-    {27, 0x00},    // $k1
-    {28, 0x00},    // $gp - global pounsigneder
-    {29, 0x00},    // $sp - stack pounsigneder
-    {30, 0x00},    // $fp - frame pounsigneder
-    {31, 0x00}     // $ra - procedure return address
-};
+#include "processor.h"
 
 unsigned get_bits(unsigned num, unsigned lsbit, unsigned msbit) {
     unsigned mask = ((1 << (msbit - lsbit + 1)) - 1) << lsbit;
     return (num & mask) >> lsbit;
+}
+
+void Processor::run(){
+    pc = 0;
+    // stores the instruction in the program counter address to cur_inst
+    unsigned cur_inst = inst_memory[pc];
+    // prepare next instruction
+    pc++;
+    // gets instruction opcode
+    opcode = get_bits(cur_inst, 26, 31);
+    // For all R-Type instructions, opcode = 0
+    if(opcode == 0){
+        rTypeInstruction(cur_inst);
+    }
+    else{
+        //iTypeInstruction(cur_inst, opcode, &pc);
+    }
 }
 
 std::bitset<32> get_binary(int num){
@@ -61,37 +28,37 @@ std::bitset<32> get_binary(int num){
     return x;
 }
 
-void add(Register *rd, Register rs, Register rt){
+void Processor::add(Register *rd, Register rs, Register rt){
     // performs the arithmetic operation
     rd->value = rs.value + rt.value;
 }
 
-void sub(Register *rd, Register rs, Register rt){
+void Processor::sub(Register *rd, Register rs, Register rt){
     // performs the arithmetic operation
     rd->value = rs.value - rt.value;
 }
 
-void and(Register *rd, Register rs, Register rt){
+void Processor::and(Register *rd, Register rs, Register rt){
     // performs the arithmetic operation
     rd->value = rs.value & rt.value;
 }
 
-void or(Register *rd, Register rs, Register rt){
+void Processor::or(Register *rd, Register rs, Register rt){
     // performs the arithmetic operation
     rd->value = rs.value | rt.value;
 }
 
-void slt(Register *rd, Register rs, Register rt){
+void Processor::slt(Register *rd, Register rs, Register rt){
     // performs the arithmetic operation
     (rs.value < rt.value) ? rd->value = 1 : rd->value = 0;
 }
 
-void beq(Register rs, Register *rt, unsigned offset, unsigned *pc){
+void Processor::beq(Register rs, Register *rt, unsigned offset, unsigned *pc){
     // compares rs to rt, if they are equal, add the offset to the program counter
     (rs.value == rt->value) ? *pc = *pc + offset : *pc = *pc;
 }
 
-void lw(Register rs, Register *rt, unsigned offset){
+void Processor::lw(Register rs, Register *rt, unsigned offset){
     // gets the value of the specified memory address
     unsigned mem_addr = rs.value + offset;
     unsigned mem_value = data_memory[mem_addr];
@@ -100,7 +67,7 @@ void lw(Register rs, Register *rt, unsigned offset){
     rt->value = mem_value;
 }
 
-void sw(Register rs, Register *rt, unsigned offset){
+void Processor::sw(Register rs, Register *rt, unsigned offset){
     // gets the value of the specified memory address
     unsigned mem_addr = rs.value + offset;
 
@@ -108,7 +75,7 @@ void sw(Register rs, Register *rt, unsigned offset){
     data_memory[mem_addr] = rt->value;
 }
 
-void rTypeInstruction(unsigned instruction){
+void Processor::rTypeInstruction(unsigned instruction){
     unsigned funct = get_bits(instruction, 0, 6);
     // parses the rd register from the instruction
     Register *rd = &registers[get_bits(instruction, 11, 15)];
@@ -136,7 +103,7 @@ void rTypeInstruction(unsigned instruction){
     }
 }
 
-void iTypeInstruction(unsigned instruction, unsigned opcode, unsigned *pc){
+void Processor::iTypeInstruction(unsigned instruction, unsigned opcode, unsigned *pc){
     // parses the rs register from the instruction
     Register rs = registers[get_bits(instruction, 21, 25)];
     // parses the rt register from the instruction
@@ -158,27 +125,11 @@ void iTypeInstruction(unsigned instruction, unsigned opcode, unsigned *pc){
 }
 
 unsigned main(){
+    Processor processor;
+
     // stores a instruction as the first instruction for testing
-    inst_memory[0] = 0x1211000b;
+    processor.inst_memory[0] = 0x02328020;
 
-    data_memory[10] = 8;
-    data_memory[9] = 8;
-    data_memory[11] = 8;
-
-    // declares program counter and sets it to 0
-    unsigned pc = 0;
-    // stores the instruction in the program counter address to cur_inst
-    unsigned cur_inst = inst_memory[pc];
-    // prepare next instruction
-    pc++;
-    // gets instruction opcode
-    unsigned opcode = get_bits(cur_inst, 26, 31);
-
-    // For all R-Type instructions, opcode = 0
-    if(opcode == 0){
-        rTypeInstruction(cur_inst);
-    }
-    else{
-        iTypeInstruction(cur_inst, opcode, &pc);
-    }
+    processor.run();
+    std::cout << processor.registers[16].value << std::endl;
 }
---
+++ processor.h
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+#define MAX_INST_MEM_SIZE 1024
+#define MAX_DATA_MEM_SIZE 65536
+
+// saves the values of all registers available
+typedef struct {
+    unsigned address;
+    unsigned value;
+} Register;
+
+class Processor {
+    public:
+        Processor();
+
+        uint32_t inst_memory[MAX_INST_MEM_SIZE];
+        uint32_t data_memory[MAX_DATA_MEM_SIZE];
+
+        Register registers[32];
+
+        // program counter
+        unsigned pc;
+        // current instruction
+        unsigned cur_inst;
+        // opcode
+        unsigned opcode;
+
+        void run();
+
+        void rTypeInstruction(unsigned instruction);
+
+        void iTypeInstruction(unsigned instruction, unsigned opcode, unsigned *pc);
+
+        void add(Register *rd, Register rs, Register rt);
+        
+        void sub(Register *rd, Register rs, Register rt);
+        
+        void and(Register *rd, Register rs, Register rt);
+        
+        void or(Register *rd, Register rs, Register rt);
+        
+        void slt(Register *rd, Register rs, Register rt);
+        
+        void beq(Register rs, Register *rt, unsigned offset, unsigned *pc);
+        
+        void lw(Register rs, Register *rt, unsigned offset);
+        
+        void sw(Register rs, Register *rt, unsigned offset);
+};
+
+Processor::Processor() : registers 
+{
+    {0, 0x00},     // $0 - constant 0
+    {1, 0x00},     // $at - assembler temporary
+    {2, 0x00},     // $v0
+    {3, 0x00},     // $v1
+    {4, 0x00},     // $a0
+    {5, 0x00},     // $a1
+    {6, 0x00},     // $a2
+    {7, 0x00},     // $a3
+    {8, 0x00},     // $t0
+    {9, 0x00},     // $t1
+    {10, 0x00},    // $t2
+    {11, 0x00},    // $t3
+    {12, 0x00},    // $t4
+    {13, 0x00},    // $t5
+    {14, 0x00},    // $t6
+    {15, 0x00},    // $t7
+    {16, 0x00},    // $s0
+    {17, 0x05},    // $s1
+    {18, 0x03},    // $s2
+    {19, 0x00},    // $s3
+    {20, 0x00},    // $s4
+    {21, 0x00},    // $s5
+    {22, 0x00},    // $s6
+    {23, 0x00},    // $s7
+    {24, 0x00},    // $t8
+    {25, 0x00},    // $t9
+    {26, 0x00},    // $k0
+    {27, 0x00},    // $k1
+    {28, 0x00},    // $gp - global pounsigneder
+    {29, 0x00},    // $sp - stack pounsigneder
+    {30, 0x00},    // $fp - frame pounsigneder
+    {31, 0x00}     // $ra - procedure return address
+} {}
